fe5b9f1523effe609ae1417867adf48250108674
xxx <93180693+George-One-Jackson@users.noreply.github.com>
2024-07-25 12:35:26 +0800

    * use unity view to dev

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,65 +1,68 @@
-import { useIntegration } from '@telegram-apps/react-router-integration';
+
 import {
-  bindMiniAppCSSVars,
-  bindThemeParamsCSSVars,
-  bindViewportCSSVars,
-  initNavigator, useLaunchParams,
-  useMiniApp,
-  useThemeParams,
+//  useLaunchParams,
   useViewport,
 } from '@telegram-apps/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
-import { type FC, useEffect, useMemo } from 'react';
-import {
-  Navigate,
-  Route,
-  Router,
-  Routes,
-} from 'react-router-dom';
-
-import { routes } from '@/navigation/routes.tsx';
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import { type FC, Fragment} from 'react';
 
 export const App: FC = () => {
-  const lp = useLaunchParams();
-  const miniApp = useMiniApp();
-  const themeParams = useThemeParams();
+
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: 'build/StageBuild.loader.js',
+    dataUrl: 'build/StageBuild.data.unityweb',
+    frameworkUrl: 'build/StageBuild.framework.js.unityweb',
+    codeUrl: 'build/StageBuild.wasm.unityweb',
+  });
+
+  //const lp = useLaunchParams();
   const viewport = useViewport();
 
-  useEffect(() => {
-    return bindMiniAppCSSVars(miniApp, themeParams);
-  }, [miniApp, themeParams]);
-
-  useEffect(() => {
-    return bindThemeParamsCSSVars(themeParams);
-  }, [themeParams]);
-
-  useEffect(() => {
-    return viewport && bindViewportCSSVars(viewport);
-  }, [viewport]);
-
-  // Create a new application navigator and attach it to the browser history, so it could modify
-  // it and listen to its changes.
-  const navigator = useMemo(() => initNavigator('app-navigation-state'), []);
-  const [location, reactNavigator] = useIntegration(navigator);
-
-  // Don't forget to attach the navigator to allow it to control the BackButton state as well
-  // as browser history.
-  useEffect(() => {
-    navigator.attach();
-    return () => navigator.detach();
-  }, [navigator]);
-
+  viewport?.expand()
+  
+  const canvasWidth = viewport?.width;
+  const canvasHeight = viewport?.height;
+  
   return (
-    <AppRoot
-      appearance={miniApp.isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
-    >
-      <Router location={location} navigator={reactNavigator}>
-        <Routes>
-          {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path='*' element={<Navigate to='/'/>}/>
-        </Routes>
-      </Router>
+    <AppRoot>
+      <Fragment>
+        <div
+          style={{
+            width: canvasWidth,
+            height: canvasHeight,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {!isLoaded ? (
+            <div
+              style={{
+                width: canvasWidth,
+                height: canvasHeight,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+            >
+              <p style={{ fontFamily: 'LanaPixel', fontSize: '38px' }}>loading...{Math.round(loadingProgression * 100)}%</p>
+
+            </div>
+          ) : null}
+          <Unity
+            unityProvider={unityProvider}
+            devicePixelRatio={devicePixelRatio}
+            style={{
+              width: canvasWidth,
+              height: canvasHeight,
+            }}
+          />
+        </div>
+      </Fragment>
     </AppRoot>
   );
 };
